fbba99188f072692bb9ebf7d3e5bb8c82f70109b
Killberos Tze <killberos.tze@gmail.com>
2025-09-04 15:25:07 +0200

    smaller cleanup

--- Hub.py
+++ Hub.py
@@ -3,12 +3,11 @@
 from tkinter import Frame, Tk, Button
 import sys
 
-
 class MultipleApps():
     def init_start(self):
         self.approot.bind("<1>", lambda event: event.widget.focus_set())
         self.approot.mainloop()
-    
+
     def __init__(self,app_list={None:None},appgeometry=(350, 400, 50, 50)):
         self.approot = Tk()
         self.appgeometry=appgeometry
@@ -30,27 +29,25 @@ class MultipleApps():
                 self.approot.geometry('%dx%d+%d+%d' % tmp.appgeometry)
             except:
                 pass
-            
-        else:        
+        else:
             self.apps_landing(self.landingframe,app_list)
 
-    
     def apps_landing(self,parent,app_list):
         row=0
         for key, item in app_list.items():
             Button(self.landingframe, text=f"{key}", command=lambda app=item,apps=app_list: self.start_app(app,apps),width=12,bg='lightgray').grid(row=row,column=1)
             row+=1
-        
+
     def single_app_landing(self,apps):
         self.sideframe=Frame(self.landingframe)
         self.sideframe.grid(row=0,column=0,sticky='NW')
         self.mainframe=Frame(self.landingframe)
         self.mainframe.grid(row=0,column=1,columnspan=10)
-        Button(self.sideframe, text="Go back", command= lambda apps=apps: self.stop_app(apps),width=4,bg='lightgray').grid(row=0,column=0)
-        
+        Button(self.sideframe, text="Go back", command= lambda apps=apps: self.stop_app(apps),width=6,bg='lightgray').grid(row=0,column=0)
+
     def placeholder(self):
         pass
-#
+
     def start_app(self,app,apps):
         self.landingframe.destroy()
         self.landingframe=Frame(self.frameroot)
@@ -62,8 +59,7 @@ class MultipleApps():
         self.approot.title(str(tmp))
         self.approot.geometry('%dx%d+%d+%d' % tmp.appgeometry)
         #Button(parent, text="Go back", command=lambda frame=parent: self.stop_app(parent),width=12,bg='lightgray').grid(row=rowcount,column=1)
-#
-        
+
     def stop_app(self,apps):
         self.landingframe.destroy()
         self.landingframe=Frame(self.frameroot)
@@ -71,11 +67,4 @@ class MultipleApps():
         self.apps_landing(self.landingframe,app_list=apps)
         self.approot.title('Application hub')
         self.approot.geometry('%dx%d+%d+%d' % self.appgeometry)
-        
-        
 
-     
-       
-#if __name__=='__main__':
-#    MultipleApps.init_start(MultipleApps())
-    
